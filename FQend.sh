f47ed5343e7de958a51a235bf971fbeaea7cd6b8
newbie <den.tynenyk@gmail.com>
2021-03-25 23:10:24 +0200

    ending function

--- FQend.sh
+++ FQend.sh
@@ -36,6 +36,63 @@ trap ctrl_c int
 			   echo -e "\n${green}[${white}${flicker}*${end}${green}] You are exiting to script. ${end}"
 			   sleep 3; clear
 			;;
-			
+
 		esac
 	}
+
+	function fakemail() {
+
+		clear; banner; sleep 1
+
+		read -p $'\033[1;32mEMAIL SPOOFER \033[0;31m> \033[0m' faker
+		clear; banner
+
+		read -p $'\033[1;32mTARGET EMAIL \033[0;31m> \033[0m' fakiado
+		clear; banner
+
+		read -p $'\033[1;32mHEAD (title) \033[0;31m> \033[0m' title
+		clear; banner
+
+		read -p $'\033[1;32mBODY MESSAGE \033[0;31m> \033[0m' message
+		sleep 1; clear; banner
+
+		echo -e "${white} The mail will send be like: \n ${end}"
+		echo -e "${red} Spoofer:${green} $faker ${end} \n"
+		echo -e "${red} Target:${green} $fakiado ${end} \n"
+		echo -e "${red} Title:${green} $title ${end} \n"
+		echo -e "${red} Message:${green} $message ${end} \n"
+
+		sleep 1
+
+		read -p $'\033[1;32m Are you sure to send this mail? \033[0;31m[y/n] > \033[0m' sure
+		case $sure in
+
+					y)
+					curl --data "destinatario=$fakiado && asunto=$title && mensaje=$message && remitente=$faker" $server
+			 clear
+			 banner
+			 sleep 1
+			 echo -e "\n${green}[${red}+${green}]${end} Sended. "
+			 		;;
+
+			 		n)
+			 clear
+			 banner
+			 exit 1
+			 		;;
+		*)	 clear; banner
+			 echo -e "\n${green}[${white}${flicker}*${end}${green}] You are exiting to script. ${end}"
+			 sleep 3
+			 		;;
+			 esac
+
+	}
+
+	function main(){
+		clear
+		banner
+		sleep 1
+		server
+	}
+
+main
